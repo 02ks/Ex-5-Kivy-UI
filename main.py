3f558e545b24396e4185a7f648ec1e14478906bf
02ks <kogansam@dpengineering.org>
2019-09-26 09:32:23 -0700

    Major Update 1.1! Features: -Joystick Screen -more UI -color changers -semi neat code

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
 import os
+from threading import Thread
+from time import sleep
+
+
 
 from kivy.app import App
 from kivy.core.window import Window
 from kivy.lang import Builder
+from kivy.properties import ObjectProperty
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.animation import Animation
 
 from pidev.MixPanel import MixPanel
 from pidev.kivy.PassCodeScreen import PassCodeScreen
 from pidev.kivy.PauseScreen import PauseScreen
+from pidev.Joystick import Joystick
 from pidev.kivy import DPEAButton
 from pidev.kivy import ImageButton
-
+import random
 MIXPANEL_TOKEN = "x"
 MIXPANEL = MixPanel("Project Name", MIXPANEL_TOKEN)
 
 SCREEN_MANAGER = ScreenManager()
 MAIN_SCREEN_NAME = 'main'
 ADMIN_SCREEN_NAME = 'admin'
+SKY_SCREEN_NAME = 'SideScreen'
+JOY_SCREEN_NAME = 'joyScreen'
 
 
 class ProjectNameGUI(App):
@@ -32,13 +41,168 @@ class ProjectNameGUI(App):
         return SCREEN_MANAGER
 
 
-Window.clearcolor = (1, 1, 1, 1)  # White
+Window.clearcolor = (0, 0, 0.1, 100)  # White
+
+xy = property(None)
+btn6 = property(None)
+lb0 = property(None)
+lbz = property(None)
+
+class JoyScreen(Screen):
+    joystick = Joystick(0, False)
+    X_axis = ObjectProperty(0.0)
+    Y_axis = ObjectProperty(0.0)
+
+
+    def __init__(self, **kwargs):
+        Builder.load_file('joyScreen')
+        super(JoyScreen, self).__init__(**kwargs)
+
+    def switch_text2(self):
+        list = [1]
+
+        if self.joystick.button_combo_check(list)==1:
+            self.lbz.text = ""
+            self.lb0.text = "Ready"
+            self.xy.color = 0, 1, 0, 1
+
+            if self.joystick.get_button_state(0) == 0:
+                self.lb0.text = "Ready"
+                self.xy.color = 0, 1, 0, 1
+            else:
+                self.lb0.text = ""
+                self.xy.color = 1, 0, 0, 1
+                self.counter()
+
+        else:
+            self.lbz.text = "Hold 2"
+            self.lb0.text = ""
+            self.xy.color = 1, 1, 1, 1
+
+
+    def joystick_thread(self):
+        while 1:
+            self.joystick.refresh()
+            self.X_axis = self.joystick.get_axis('x') * Window.size[0] * 1/2
+            self.Y_axis = self.joystick.get_axis('y') * Window.size[1] * 1/2
+            # self.switch_text()
+            self.switch_text2()
+            sleep(1/200)
+            # print(self.X_axis)
+
+    def start_joystick_thread(self):
+        Thread(target=self.joystick_thread).start()
+
+    def transition_back(self):
+        """
+        Transition back to the main screen
+        :return:
+        """
+        SCREEN_MANAGER.current = MAIN_SCREEN_NAME
+
+    global xin
+    xin = 0
+    global x
+
+    def counter(self):
+        global xin
+        global x
+        xin = xin + 1
+        if xin == 1000:
+            xin = 0
+        else:
+            print ("%d" % xin)
+            x = "%d" % xin
+            random.seed(x)
+            Window.clearcolor = (random.random(), random.random(), random.random(), 1000)
+
+
+
+
+
+
+test_button3 = property(None)
+fly2 = property(None)
+
+class SideScreen(Screen):
+    def __init__(self, **kwargs):
+        Builder.load_file('sideScreen.kv')
+        super(SideScreen, self).__init__(**kwargs)
+        anim = Animation(x=50) + Animation(size=(180, 80)) + Animation(x=90) + Animation(size=(150, 150)) + Animation(
+            y=50) + Animation(y=150)
+        anim.repeat = True
+        anim.start(self.fly2)
+
+    def transition_back(self):
+        """
+        Transition back to the main screen
+        :return:
+        """
+        SCREEN_MANAGER.current = MAIN_SCREEN_NAME
+
+    def exit_program(self):
+        """
+        Quit the program. This should free all steppers and do any cleanup necessary
+        :return: None
+        """
+        quit()
+
+    def random(self):
+        if self.test_button3.text == "o-\<":
+            self.test_button3.text = "o-/<"
+
+        elif self.test_button3.text == "o-/<":
+            self.test_button3.text = "o-\<"
 
 
 class MainScreen(Screen):
     """
     Class to handle the main screen and its associated touch events
     """
+
+    btn1 = property(None)
+    btn2 = property(None)
+    btn3 = property(None)
+    fly = property(None)
+
+    def __init__(self, **kwargs):
+        Builder.load_file('sideScreen.kv')
+        super(MainScreen, self).__init__(**kwargs)
+        anim = Animation(x=50) + Animation(size=(80, 80)) + Animation(x=90) + Animation(size=(150, 150)) + Animation(y=50) + Animation(y=100)
+        anim.repeat = True
+        anim.start(self.fly)
+
+
+    def next(self):
+        SCREEN_MANAGER.current = SKY_SCREEN_NAME
+
+    def next2(self):
+        SCREEN_MANAGER.current = JOY_SCREEN_NAME
+    def affect(self):
+        self.btn3.text = "Changing..."
+        if self.lb1.text == "Motor On":
+            self.lb1.text = "Motor Off"
+
+        elif self.lb1.text == "Motor Off":
+            self.lb1.text = "Motor On"
+    def switch_text(self):
+
+        if self.btn1.text == "On":
+            self.btn1.text = "Off"
+
+        elif self.btn1.text == "Off":
+            self.btn1.text = "On"
+
+    global xin
+    xin = 0
+    def counter(self):
+        global xin
+        xin = xin + 1
+        self.btn2.text = "%d" % xin
+
+
+
+
 
     def pressed(self):
         """
@@ -106,6 +270,8 @@ SCREEN_MANAGER.add_widget(MainScreen(name=MAIN_SCREEN_NAME))
 SCREEN_MANAGER.add_widget(PassCodeScreen(name='passCode'))
 SCREEN_MANAGER.add_widget(PauseScreen(name='pauseScene'))
 SCREEN_MANAGER.add_widget(AdminScreen(name=ADMIN_SCREEN_NAME))
+SCREEN_MANAGER.add_widget(SideScreen(name=SKY_SCREEN_NAME))
+SCREEN_MANAGER.add_widget(JoyScreen(name=JOY_SCREEN_NAME))
 
 """
 MixPanel
